985724c2ea434f6064950cfc930ccd3843d24121
Kyle <kylesch115@gmail.com>
2025-01-16 11:46:19 -0700

    flag to specify graphics rendering mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
 #include <Game.hpp>
 #include <RasterRenderer.hpp>
 #include <Renderer.hpp>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
+  // temporary
+  bool useRasterGraphics = false;
+  if (argc > 1)
+  {
+    for (int i = 1; i < argc; ++i)
+    {
+      std::string arg = argv[i];
+
+      if (arg[0] == '-')
+      {
+        if (arg[1] == 'r')
+        {
+          useRasterGraphics = true;
+        }
+      }
+    }
+  }
+
   Game game;
-  // Renderer renderer;
-  RasterRenderer renderer;
-  renderer.run(game);
+
+  if (useRasterGraphics)
+  {
+    RasterRenderer renderer;
+    renderer.run(game);
+  }
+  else
+  {
+    Renderer renderer;
+    renderer.run(game);
+  }
+
   return 0;
 }
